344040521e58761a5d00573287c49aff5309ced7
Sharvan Sharma <itbhu.sk@gmail.com>
2025-03-21 14:16:53 +0530

    Automated commit: backend/models/Image.js

---
+++ backend/models/Image.js
@@ -0,0 +1,1 @@
+const mongoose = require('mongoose'); const ImageSchema = new mongoose.Schema({ url: { type: String, required: true }, description: { type: String, required: true }, createdAt: { type: Date, default: Date.now } }); module.exports = mongoose.model('Image', ImageSchema);
